309603acc6082d6fdb65dca87695d63f4d6e7961
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-07-12 09:03:37 +0000

    nique

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -5,7 +5,7 @@ void my_print_n_ascii(int n)
     if (n > 94) {
         return;
     }
-    for (int i = 33; i <= n; i++) {
+    for (int i = 33; i <= 33 + n; i++) {
         print_char(i);
     }
     
